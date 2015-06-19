2eefeda895084506bba48f712ea789e8fea1d0d3
David Gerding <dave@versive.com>
2015-06-19 14:57:56 -0500

    Trying net 4.52 target.  Seems to work also.  Figuring out best embedded resource strategy.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenNLP;
+using OpenNLP.Tools;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+
+        private string mModelPath= @"C:\Projects\DotNet\OpenNLP\OpenNLP\Models";
+
+        private OpenNLP.Tools.SentenceDetect.MaximumEntropySentenceDetector mSentenceDetector;
+
+        public void Main(string[] args)
+        {
+            if (mSentenceDetector == null)
+            {
+                mSentenceDetector = new OpenNLP.Tools.SentenceDetect.EnglishMaximumEntropySentenceDetector(mModelPath + "EnglishSD.nbin");
+            }
+
+            var test = mSentenceDetector.SentenceDetect("Hi! I'm three sentences.  Are you?");
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        private static string mModelPath = @"C:\Projects\DotNet\OpenNLP\OpenNLP\Models\";
+        private static OpenNLP.Tools.SentenceDetect.MaximumEntropySentenceDetector mSentenceDetector;
+
+        static void Main(string[] args)
+        {
+            if (mSentenceDetector == null)
+            {
+                mSentenceDetector = new OpenNLP.Tools.SentenceDetect.EnglishMaximumEntropySentenceDetector(mModelPath + "EnglishSD.nbin");
+            }
+
+            var test = mSentenceDetector.SentenceDetect("Not a chance. Hi! Mr. Arnold L. Watson is a big guy. I'm three sentences.  Are you?");
+        }
+    }
+}
+
+
